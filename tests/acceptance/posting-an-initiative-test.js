6816cf7a0d8f5359abe763eb728b3cbeee2046f2
Qianfinland <qianfinland@gmail.com>
2014-07-30 19:25:31 +0300

    reminder of from-filling on description and submit

--- tests/acceptance/posting-an-initiative-test.js
+++ tests/acceptance/posting-an-initiative-test.js
@@ -18,6 +18,18 @@ test('Successfully', function(){
       expect(currentURL()).to.equal('/initiatives/new');
       // fill in title after user clicks the link
       fillIn('div.title input', 'Public health clinic');
+      //add reminder of form-filling
+      fillIn('div.description textarea', 'Allocate compensation money to create a local public health clinic');
+      click('form input[type=submit]').then(function() {
+        // Ideally I'd prefer to:
+        //   expect(currentURL()).to.equal('/initiatives/' + initiativeID);
+        // but haven't found an ember equivalent of active record's Post.first.id,
+        // nor a way to get at the local store and use store.all('initiative').objectAt(0).get('id').
+        // Also can't get a record count yet
+        expect(currentPath()).to.equal('initiatives.show');
+        expect(find('.title').text()).to.equal('Public health clinic');
+        expect(find('.description').text()).to.equal('Allocate compensation money to create a local public health clinic');
+      });
     });
   });
 });
